6598ed906c0c10e068ed7d3df344eb9c1ea08003
Sara Hopper <sara.hopper@gmail.com>
2017-10-01 00:06:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,60 +1,96 @@
-let store = {customers: [], employers: [], meals: [], deliveries: []}
+let store = {deliveries: [], employers: [], customers: [], meals: []}
 
-let customerId = 0
+let mealId = 0
 
-class Customer {
-  constructor(name, employer = {}){
-    this.id = ++customerId
-    this.name = name
-    this.employerId = employer.id
-
-    store.customers.push(this)
+class Meal {
+  constructor(title, price){
+    this.title = title
+    this.price = price
+    this.id = ++mealId
+    store.meals.push(this)
   }
-
   deliveries(){
     return store.deliveries.filter((delivery)=> {
-      return delivery.customerId === this.id
+      return delivery.mealId == this.id
     })
   }
+  customers(){
+    return this.deliveries().map((delivery)=> {
+      return delivery.customer()
+    })
+  }
+  static byPrice(){
+    return store.meals.sort((meal) => { return meal.price });
+  }
+}
 
+let customerId = 0
+class Customer {
+  constructor(name, employer = {} ){
+    this.name = name;
+    this.employerId = employer.id;
+    this.id = ++customerId
+    store.customers.push(this)
+  }
+  totalSpent(){
+    return this.meals().reduce(function(sum, meal) { return sum + meal.price }, 0)
+  }
+  deliveries(){
+    return store.deliveries.filter((delivery)=> {
+      return delivery.customerId == this.id
+    })
+  }
   meals(){
     return this.deliveries().map((delivery)=> {
       return delivery.meal()
     })
   }
-
-  totalSpent(){
-	    return this.meals().reduce(function(sum, meal) { return sum + meal.price }, 0)
-	}
 }
+
+
+let deliveryId = 0
+class Delivery {
+  constructor(meal = {}, customer = {}){
+    this.mealId = meal.id;
+    this.customerId = customer.id;
+    this.id = ++deliveryId
+    store.deliveries.push(this)
+  }
+  meal(){
+    return store.meals.find((meal) => { return meal.id === this.mealId })
+  }
+  customer(){
+    return store.customers.find((customer) => { return customer.id === this.customerId })
+  }
+}
+
 
 let employerId = 0
 
 class Employer {
   constructor(name){
-    this.id = ++employerId
     this.name = name
-
+    this.id = ++employerId
     store.employers.push(this)
   }
-
   mealTotals(){
-    let mealsTotals = {}
-    let keys = this.meals().map( meal =>  meal.id)
-    let mealsId = this.deliveries().map( delivery => delivery.mealId )
-    for (let i = 0; i < keys.length; i++){
-      mealsTotals[keys[i]] = mealsId.filter( mealId => { return mealId === keys[i] }).length
-    }
-
-    return mealsTotals
+    let allMeals = this.deliveries().map((delivery) => {
+      return delivery.meal()
+    })
+    let summaryObject = {}
+    allMeals.forEach(function(meal){
+      summaryObject[meal.id] = 0
+    })
+    allMeals.forEach(function(meal){
+      summaryObject[meal.id] += 1
+    })
+    return summaryObject;
   }
-
   employees(){
     return store.customers.filter((customer)=> {
       return customer.employerId == this.id
     })
   }
-
   deliveries(){
     let allDeliveries = this.employees().map((employee)=> {
       return employee.deliveries()
@@ -62,61 +98,11 @@ class Employer {
     let merged = [].concat.apply([], allDeliveries);
     return merged;
   }
-
   meals(){
     let allMeals = this.deliveries().map((delivery)=> {
       return delivery.meal()
     })
     let uniqueMeals = [...new Set(allMeals)]
     return uniqueMeals;
-  }
-
-}
-
-let mealId = 0
-
-class Meal {
-  constructor(title, price){
-    this.id = ++mealId
-    this.title = title
-    this.price = price
-
-    store.meals.push(this)
-  }
-
-  deliveries(){
-    return store.deliveries.filter((delivery) => {
-      return delivery.mealId == this.id
-    })
-  }
-
-  customers(){
-    return this.deliveries().map((delivery) => {
-      return delivery.customer()
-    })
-  }
-
-  static byPrice(){
-    return store.meals.sort((meal) => { return meal.price });
-  }
-}
-
-let deliveryId = 0
-
-class Delivery {
-  constructor(meal ={}, customer ={}){
-    this.id = ++deliveryId
-    this.mealId = mealId
-    this.customerId = customerId
-
-    store.deliveries.push(this)
-  }
-
-  meal(){
-    return store.meals.find((meal) => { return meal.id === this.mealId })
-  }
-
-  customer(){
-    return store.customers.find((customer) => { return customer.id === this.customerId })
   }
 }
